13c952f0d51d6b585c437128f0fb0895499a6760
Artem-Strekalov <48091807+Artem-Strekalov@users.noreply.github.com>
2020-09-29 10:42:05 +0400

    the change

--- src/components/AuthorizationPage.vue
+++ src/components/AuthorizationPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div class="block">  
+      <h1>sas</h1>
     </div>
   </div>
 </template>
@@ -11,9 +12,9 @@ export default {
 };
 </script>
 <style lang="scss">
-.block {
-height: 768px;
+body{
 background: radial-gradient(63.88% 129.61% at 25.55% 31.91%, #20135C 0%, #140739 92.82%);
 box-shadow: 0px 15px 40px rgba(0, 0, 0, 0.25);
 }
+
 </style>
